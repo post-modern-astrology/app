eb00795faa28388b8de656745365e83d006c4dd6
post-modern-astrology <xeno.sama@icloud.com>
2025-02-22 15:11:25 +0600

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0a8bb77f26faf26ef3c68db262d4c04d",
+const RESOURCES = {"flutter_bootstrap.js": "d16f5d701f8403c3ee987c7fe4bdb40d",
 "version.json": "3d2ae1201680513a199a7a8b5dbc2c48",
 "index.html": "f0cd766c285b68601eb69b637a006dde",
 "/": "f0cd766c285b68601eb69b637a006dde",
-"main.dart.js": "25f2eb0d8a7bd530993e7248ea0d69f1",
+"main.dart.js": "7c76e6294154def28f0951793d71064d",
 "sqlite3.wasm": "78498ab7d022b0069cdf876adf5b1515",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "c3892348c2dc9bc48e53bf1628299749",
