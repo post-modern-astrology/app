5db2ec22e15a5b3c7fbb371d2807114b549d31e6
post-modern-astrology <xeno.sama@icloud.com>
2025-03-25 17:51:11 +0600

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4071577008"
+    serviceWorkerVersion: "2579219854"
   }
 });
