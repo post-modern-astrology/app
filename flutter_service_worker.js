ee52a60710288eedfeab9f901af36a17429959c9
post-modern-astrology <xeno.sama@icloud.com>
2025-08-22 15:36:56 +0600

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,20 +3,18 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "35df468914bee8c96d6aff4d10c8705a",
+const RESOURCES = {"flutter_bootstrap.js": "cddf0fe89c78af484d5ce9d27daa5811",
 "version.json": "a853487c22af87ef05e2b98828b87147",
 "index.html": "f0d1731ec6a2fc071ddcb9585ec171a8",
 "/": "f0d1731ec6a2fc071ddcb9585ec171a8",
 "main.dart.js": "2397fd54f94405e6754cdaefd579d718",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.mjs": "83c2a53493317b701e4eebb042079a8d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "0ba6a44fd0f22770741b1e7e7484332d",
-"main.dart.wasm": "25bba0139ef5ed78010564d9ba3b3d1c",
 "assets/AssetManifest.json": "356950b185d5e1ef1224eb012b5ed2e1",
 "assets/NOTICES": "ed01fbd7b38457dd56639a90db246cc1",
 "assets/FontManifest.json": "67a28da3784fc091c2f816d615fbf08a",
@@ -97,8 +95,6 @@ const RESOURCES = {"flutter_bootstrap.js": "35df468914bee8c96d6aff4d10c8705a",
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
-"main.dart.wasm",
-"main.dart.mjs",
 "index.html",
 "flutter_bootstrap.js",
 "assets/AssetManifest.bin.json",
