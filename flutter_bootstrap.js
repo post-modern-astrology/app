491bb44784adaed3922fee3946e38f559a109344
Ruslan.D <x.stelvia@gmail.com>
2025-09-03 18:11:58 +0600

    f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "331946891"
+    serviceWorkerVersion: "317222299"
   }
 });
